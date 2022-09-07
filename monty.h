27b5bcdbe036c4c1dd7344345fa2437636f3e4a8
sualah <sabdullai@acsint.io>
2022-09-07 20:57:44 +0000

    chore(app):stacks implementation

--- monty.h
+++ monty.h
@@ -1,10 +1,31 @@
-#ifndef MONTY_H_
-#define MONTY_H_
+#ifndef _MONTY_H_
+#define _MONTY_H_
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * struct var_s - struct to contain the main variables of the Monty interpreter
+ * @queue: flag to determine if in stack vs queue mode
+ * @stack_len: length of the stack
+ */
+typedef struct var_s
+{
+	int queue;
+	size_t stack_len;
+} var_t;
+
+#define STACK 0
+#define QUEUE 1
+
+/* global struct to hold flag for queue and stack length */
+extern var_t var;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,7 +33,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Coding project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -20,75 +41,43 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Coding project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/**
- * struct var - temporarily variable and checker
- * @temp: temporarily variable
- * @check: check variable
- *
- * Description: store temp variable an check
- */
-typedef struct var
-{
-	int temp;
-	char check;
-} var_t;
 
-extern var_t variables;
-var_t variables;
+void get_op(char *op, stack_t **stack, unsigned int line_number);
+void m_push(stack_t **stack, unsigned int line_number);
+void m_push2(stack_t **stack, int n);
+void m_pall(stack_t **stack, unsigned int line_number);
+void m_pint(stack_t **stack, unsigned int line_number);
+void m_pop(stack_t **stack, unsigned int line_number);
+void m_swap(stack_t **stack, unsigned int line_number);
+void m_add(stack_t **stack, unsigned int line_number);
+void m_nop(stack_t **stack, unsigned int line_number);
+void m_sub(stack_t **stack, unsigned int line_number);
+void m_mul(stack_t **stack, unsigned int line_number);
+void m_div(stack_t **stack, unsigned int line_number);
+void m_mod(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void m_stack(stack_t **stack, unsigned int line_number);
+void m_queue(stack_t **stack, unsigned int line_number);
+void m_pchar(stack_t **stack, unsigned int line_number);
+void m_pstr(stack_t **stack, unsigned int line_number);
+void free_stack(int status, void *arg);
+void m_fs_close(int status, void *arg);
+void free_lineptr(int status, void *arg);
+stack_t *add_node(stack_t **stack, const int n);
 
-#define DELIM " \n"
-
-void readfile(FILE *filename, stack_t **stk, unsigned int line_number);
-
-void tokenizer(char *line, stack_t **stk, unsigned int line_number);
-
-void findOps(char *token, stack_t **stk, unsigned int line_number);
-
-void free_stk(stack_t **stk, unsigned int line_number);
-
-void push(stack_t **stk, unsigned int line_number);
-
-void pall(stack_t **stk, unsigned int line_number);
-
-void pop(stack_t **stk, unsigned int line_number);
-
-void swap(stack_t **stk, unsigned int line_number);
-
-void pint(stack_t **stk, unsigned int line_number);
-
-void add(stack_t **stk, unsigned int line_number);
-
-void sub(stack_t **stk, unsigned int line_number);
-
-void _div(stack_t **stk, unsigned int line_number);
-
-void _mul(stack_t **stk, unsigned int line_number);
-
-void _mod(stack_t **stk, unsigned int line_number);
-
-int is_num(char *token);
-
-void _nop(stack_t **stk, unsigned int line_number);
-
-void pchar(stack_t **stk, unsigned int line_number);
-
-void pstr(stack_t **stk, __attribute__((unused))unsigned int line_number);
-
-void rotl(stack_t **stk, __attribute__((unused))unsigned int line_number);
-
-void rotr(stack_t **stk, __attribute__((unused))unsigned int line_number);
-
-#endif
+#endif /* _MONTY_H_ */
